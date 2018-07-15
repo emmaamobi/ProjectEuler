71f950c6aec7cfbbec2200ea3ac11d800d981c8c
AmobiSomto <amobisomto@gmail.com>
2018-07-15 17:32:02 -0500

    Solved problem 6

--- src/Euler5.java
+++ src/Euler5.java
@@ -1,7 +1,18 @@
 
 public class Euler5 {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+//		System.out.println("Hello world");
+		int largest = 0;
+		int[] nums = new int[10];
+		for (int i = 0; i<nums.length;i++) {
+			nums[i] = i+1;
+		}
+		for (int i= 0;i<nums.length;i++) {
+			System.out.println(nums[i]);
+		}
+		
+		
+	
 		
 	}
 
--- src/Euler6.java
+++ src/Euler6.java
@@ -1,14 +1,13 @@
 
 public class Euler6 {
 	public static void main (String[] args) {
-		System.out.println("Hello");
-//		int sumSq = 0;
-//		int sqSum = 0;
-//		for (int i = 1; i<101;i++) {
-//			sumSq += i*i;
-//			sqSum += i;
-//		}
-//		sqSum = sqSum * sqSum;
-//		System.out.println(sqSum);
+		int sumSq = 0;
+		int sqSum = 0;
+		for (int i = 1; i<101;i++) {
+			sumSq += i*i;
+			sqSum += i;
+		}
+		sqSum = sqSum * sqSum;
+		System.out.println(sqSum - sumSq);
 	}
 }
