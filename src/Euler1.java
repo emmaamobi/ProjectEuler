3cf11c8b47978cbce94a2a3d8915822ad5733984
eamobi <amobisomto@gmail.com>
2019-05-22 18:42:14 -0500

    updated problem 1

--- src/Euler1.java
+++ src/Euler1.java
@@ -1,3 +1,8 @@
+import java.util.stream.IntStream;
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
 public class Euler1 {
 	public static void main (String[] args) {
 		int sum = 0;
@@ -7,6 +12,9 @@ public class Euler1 {
 			}
 		}
 		System.out.println(sum);
+		// using new Java streams
+		System.out.println(IntStream.range(1,1000).filter(x -> x % 3 == 0 || x % 5 == 0).sum());
+		
 
 	}
 
